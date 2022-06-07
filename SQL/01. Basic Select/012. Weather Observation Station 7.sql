ef22f30cda331c2c77157c067b732fcaf2e6c240
Surraiya <104488515+Surraiya@users.noreply.github.com>
2022-06-07 16:15:08 +0600

    Update 012. Weather Observation Station 7.sql

--- SQL/01. Basic Select/012. Weather Observation Station 7.sql
+++ SQL/01. Basic Select/012. Weather Observation Station 7.sql
@@ -1,7 +1,12 @@
 -- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-7/problem
 -- # Score: 10
 
-
+--#Using Regular Expression
 SELECT DISTINCT CITY
 FROM STATION
-WHERE REGEXP_LIKE(City, '[aeiou]$');
+WHERE CITY REGEXP '[aeiou]$';
+
+--#Using Right()
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) IN ('a','e','i','o','u') 
