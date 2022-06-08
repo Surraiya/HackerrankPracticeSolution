dd3dd5a35a309511a42877c3d028e706b4b28a47
Surraiya <104488515+Surraiya@users.noreply.github.com>
2022-06-08 12:32:25 +0600

    Update 017. Weather Observation Station 20.sql

--- SQL/03. Aggregation/017. Weather Observation Station 20.sql
+++ SQL/03. Aggregation/017. Weather Observation Station 20.sql
@@ -1,6 +1,11 @@
 -- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-20/problem
 -- # Score: 40
 
-
-SELECT ROUND(MEDIAN(Lat_N), 4)
-FROM Station;
+--# MySQL doesn't have median function therefore we use a variable rowindex then sort a table in a ascending order where the value of rowindex is the total rows of the table 
+--# if the rows is even number then in where condition it'll return one row as median and if it is odd number then it'll return two rows. Then we will avg the LAT_N value of that two rows.
+set @rowindex := -1;
+select round(avg(s.LAT_N),4)
+from (select @rowindex:= @rowindex + 1 as row_index, LAT_N
+from station
+order by LAT_N) as s
+where s.row_index in (floor(@rowindex/2),ceil(@rowindex/2)) 
